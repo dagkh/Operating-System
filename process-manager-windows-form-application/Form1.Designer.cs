b4070053159fe9f5f2aa0dd0db0f255047ba90e0
dagkh <khoanguyenpt123@gmail.com>
2022-12-05 12:26:48 +0700

    add all process-manager-windows-form-application project

--- process-manager-windows-form-application/Form1.Designer.cs
+++ process-manager-windows-form-application/Form1.Designer.cs
@@ -29,12 +29,123 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbOutput = new System.Windows.Forms.ListBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(13, 13);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(178, 49);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(197, 24);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(592, 26);
+            this.txtPath.TabIndex = 3;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(795, 13);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(166, 49);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Các process ₫ã ₫ược kích hoạt và ₫ang chạy:";
+            // 
+            // lbOutput
+            // 
+            this.lbOutput.FormattingEnabled = true;
+            this.lbOutput.ItemHeight = 20;
+            this.lbOutput.Location = new System.Drawing.Point(13, 111);
+            this.lbOutput.Name = "lbOutput";
+            this.lbOutput.Size = new System.Drawing.Size(947, 424);
+            this.lbOutput.TabIndex = 6;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(16, 561);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(175, 49);
+            this.btnRefresh.TabIndex = 7;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(795, 561);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(165, 49);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(973, 622);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.lbOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Process Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbOutput;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnStop;
     }
 }
 
--- process-manager-windows-form-application/Form1.cs
+++ process-manager-windows-form-application/Form1.cs
@@ -7,14 +7,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace process_manager_windows_form_application
 {
     public partial class Form1 : Form
     {
+        void DisplayProcess()
+        {
+            // Xóa nội dung cũ của ListBox
+            lbOutput.Items.Clear();
+
+            // Tìm danh sách các Process đang chạy.
+            Process[] plist = Process.GetProcesses();
+
+            // Hiển thị tên, ID của từng Process
+            foreach (Process process in plist)
+            {
+                lbOutput.Items.Add(process.ProcessName + "," + process.Id);
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            DisplayProcess();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            // Tạo form duyệt chọn file khả thi cần chạy.
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            // Hiển thị form duyệt chọn file khả thi cần chạy.
+            DialogResult ret = dlg.ShowDialog();
+
+            // Kiểm tra quyết định của user, nếu user chọn OK thì ghi nhận tên file.
+            if (ret == DialogResult.OK)
+                txtPath.Text = dlg.FileName;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Tạo mới đối tượng quản lý Process.
+            Process myProcess = new Process();
+            try
+            {
+                // Thiết lập đường dẫn file cần chạy. 
+                myProcess.StartInfo.FileName = txtPath.Text;
+
+                // Kích hoạt process
+                myProcess.Start();
+
+                // Hiển thị danh sách các process đang chạy.
+                DisplayProcess();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            // Hiển thị lại danh sách các Process đang hiện hành.
+            DisplayProcess();
         }
     }
 }
